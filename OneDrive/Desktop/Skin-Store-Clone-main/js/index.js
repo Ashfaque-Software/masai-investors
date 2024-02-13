417c8acf1a30f08258a15225709f942f346f4f7d
Ashfaque_Ansari <ashfaqueansari101@gmail.com>
2024-02-13 16:20:10 +0530

    added html

---
+++ OneDrive/Desktop/Skin-Store-Clone-main/js/index.js
@@ -0,0 +1,42 @@
+//let num =5;
+
+//if(num % 3==0){
+  //  console.log("number is devisible by 3")
+//}else {
+  //  console.log("false")
+//}
+
+// If a person is allowed to drive in India print "Apply for a license" or "NA".
+
+//let age=19;
+//if(age=>18){
+  //  console.log("Apply for license")
+//}else {
+  //  console.log("NA")
+//}
+ 
+//Given 2 numbers a and b print which is greater or "both equal".
+//let Number1=10;
+//let Number2=12;
+//if(Number1>Number2){
+  //  console.log("Number1 is greater then Number2")
+//}else if(Number1<Number2){
+  //  console.log("Number 2 is greater than Number 1")
+//}else {
+  //  console.log("both are equal")
+//}
+
+//Problem 4: Given stored username and password and input username and password, Print if the user can login or not.
+const  storedUsername="ashfaque";
+const  storedPassword="ashking";
+
+const inputUsername="ashfaque";
+const inputPassword="ashking";
+
+if(storedUsername==inputUsername && storedPassword==inputPassword){
+    console.log("you can login")
+}else{
+    console.log("wrong creadentials")
+}
+
+
